8ce7a1b7ad354ab02d13a03c4fd1abbe9e462e0e
root <alanolvera_2000@hotmail.com>
2022-02-08 18:56:10 -0600

    Agregamos el JS al HTML para hacer la pagina interactiva

--- descuentos.js
+++ descuentos.js
@@ -8,11 +8,15 @@ function calcularPrecioDescuento(precio, descuento){
     return precioDescuento;
 }
 
+function priceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
 
+    const precioDescuento = calcularPrecioDescuento(priceValue, discountValue);
 
-// console.log({
-//     precioOriginal,
-//     descuento,
-//     porcentajePrecioConDescuento,
-//     precioDescuento
-// });
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioDescuento;
+}
