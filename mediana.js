66e051077044e20c961ceabb1cb3b9c87214d54d
root <alanolvera_2000@hotmail.com>
2022-02-15 18:05:30 -0600

    Cambios y agragamos pagina de calculo salarial

--- mediana.js
+++ mediana.js
@@ -1,40 +1,32 @@
-function calcularMediaAritmetica(lista){
-    const sumaLista = lista.reduce(
-        function (valorAcumulado = 0, nuevoElemento){
-            return valorAcumulado + nuevoElemento;
-        }, 0);
-
-    const promedioLista = sumaLista / lista.length;
-    return promedioLista;
+function calcularMediaAritmetica(lista) {
+  const sumaLista = lista.reduce(function (valorAcumulado = 0, nuevoElemento) {
+    return valorAcumulado + nuevoElemento;
+  });
+  const promedioLista = sumaLista / lista.length;
+  return promedioLista;
 }
 
-const lista1 = [
-    100,
-    200, 
-    400,
-    10000,
-]
+const lista1 = [100, 200, 400, 10000];
 
-const   mitadLista1 = parseInt(lista1.length / 2);
+const mitadLista1 = parseInt(lista1.length / 2);
 
-function esPar(numero){
-    if(numero % 2 === 0){
-        return true;
-    }
-    else {
-        return false;
-    }
+function esPar(numero) {
+  if (numero % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 let mediana;
 
-if (esPar(lista1.length)){
-    const elemento1 = lista1[mitadLista1];
-    const elemento2 = lista1[mitadLista1 - 1];
+if (esPar(lista1.length)) {
+  const elemento1 = lista1[mitadLista1];
+  const elemento2 = lista1[mitadLista1 - 1];
 
-    const promedioElementos = calcularMediaAritmetica(elemento1,elemento2);
+  const promedioElementos = calcularMediaAritmetica(elemento1, elemento2);
 
-    mediana = promedioElementos;
+  mediana = promedioElementos;
 } else {
-    mediana = lista1[mitadLista1];
+  mediana = lista1[mitadLista1];
 }
--- promedio.js
+++ promedio.js
@@ -1,17 +1,9 @@
 const lista;
 
-function calcularMedia(lista) {
-  //   let sumaLista = 0;
-
-  //   for (let i = 0; i < lista.length; i++) {
-  //    sumaLista = sumaLista + lista[i];
-  //   }
-
+function calcularMediaAritmetica(lista) {
   const sumaLista = lista.reduce(function (valorAcumulado = 0, nuevoElemento) {
     return valorAcumulado + nuevoElemento;
   });
-
   const promedioLista = sumaLista / lista.length;
-
   return promedioLista;
 }
